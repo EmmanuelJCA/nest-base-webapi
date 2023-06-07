c6a38c2aa6919609c53c50749df316ae982ab8f0
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-06-07 13:23:37 -0400

    initial config

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  // App global config
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('Base RESTful API')
+    .setDescription('Base endpoints')
+    .setVersion('1.0')
+    .addSecurity('bearer', { type: 'http', scheme: 'bearer' })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
+}
+bootstrap();
